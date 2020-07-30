cec45e4174db7c0c544d67025cd9af6b21da3982
Lotsmon <41575004+MrLotsmon@users.noreply.github.com>
2020-07-30 10:06:58 +1000

    Delete connect.js

--- cmds/music/connect.js
+++
@@ -1,8 +0,0 @@
-const discord = require("discord.js");
-const bot = require(`${process.cwd()}/bot.js`);
-
-module.exports.run = async message => {
-    const voiceChannel = message.member.voiceChannel;
-    if(!voiceChannel) return message.reply('Join voice channel!');
-    voiceChannel.join();
-};
